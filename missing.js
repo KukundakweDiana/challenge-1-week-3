a356121daf7fccefd51140b6f994d41746b8cce4
Kukundakwe Diana <52125203+KukundakweDiana@users.noreply.github.com>
2019-12-12 19:16:05 +0300

    Add files via upload

--- missing.js
+++ missing.js
@@ -1,8 +1,24 @@
-var counting_numbers = [1, 2, 3, 5, 7, 9, 10];
-var missing_numbers = [];
-for (var i = 0; i < counting_numbers.length; i++) {
-  if (counting_numbers[i + 1] - counting_numbers[i] > 1) {
-    missing_numbers.push(counting_numbers[i + 1] - counting_numbers[1]);
+// var counting_numbers = [1, 3];
+// var missing_numbers = [];
+// for (var i = 0; i < counting_numbers.length; i++) {
+//   if (counting_numbers[i + 1] - counting_numbers[i] !== 1) {
+//     missing_numbers.push(counting_numbers[i] + 1);
+//   }
+// }
+// console.log(missing_numbers);
+// module.exports = counting_numbers;
+// var counting_numbers = [1, 2, 3, 5, 6, 8, 10];
+function missing(list) {
+  var missing_numbers = [];
+  var max = Math.max(...list);
+  var min = Math.min(...list);
+  for (var n = min; n <= max; n++) {
+    if (list.includes(n)) {
+    } else {
+      missing_numbers.push(n);
+    }
   }
+  return missing_numbers;
 }
-console.log(missing_numbers);
+
+module.exports = missing;
